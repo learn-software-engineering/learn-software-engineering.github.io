bcf1201d033732bb68e2356a77972793daf59e15
learn-software-engineering <learn.software.eng@gmail.com>
2022-10-29 23:08:45 +0000

    Publish revision 6d12dcdad9fe63fd8da462fdd39821049ca53505

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,13 +1,1 @@
-var store = [{
-        "title": "Post: Modified Date",
-        "excerpt":"This post has been updated and should show a modified date if used in a layout.   First paragraph to test.   Second paragraph.   Third paragraph.   Fourth paragraph.   Fifth paragraph.  ","categories": ["Blog"],
-        "tags": ["Post Formats","readability","standard"],
-        "url": "/blog/post-modified/",
-        "teaser": null
-      },{
-        "title": "Post: Standard with long title to test overlay image",
-        "excerpt":"First paragraph to test.     This post has a manual excerpt &lt;!--more--&gt; set after the second paragraph. The following YAML Front Matter has also be applied:   excerpt_separator: \"&lt;!--more--&gt;\"   Section 1   Second paragraph.   Section 1.1   Third paragraph.   Section 1.1.1   Fourth paragraph.   Section 1.1.2   Fifth paragraph.   Section 1.2   Sixth paragraph.   Section 2   Seventh paragraph.   Section 2.1   Eighth paragraph.   Section 2.2   Ninth paragraph.   Section 2.2.1   Tenth paragraph.   Section 2.2.2   Eleventh paragraph.  ","categories": ["Blog"],
-        "tags": ["Post Formats","readability","standard"],
-        "url": "/blog/post-standard/",
-        "teaser": null
-      }]
+var store = []
